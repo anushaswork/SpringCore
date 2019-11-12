e4e9f3e7dab6efa35aee9b69ac39e6965460b0c6
Anusha Srinivasan <anushaswork@gmail.com>
2019-11-12 08:53:27 +0530

    Spring MVC First Commit

---
+++ SpringMVC/home.com/src/mycafe/controllers/MyCafeControllers.java
@@ -0,0 +1,29 @@
+package mycafe.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.HttpRequestHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyCafeControllers {
+	
+	@RequestMapping("/welcome")
+	public String showWelcomePage(Model model)
+	{
+		//sending a value to view JSP
+		return "Welcome";
+	}
+	
+	@RequestMapping("processOrder")
+	public String processOrder(HttpServletRequest request,Model model)
+	{
+		//send the user entered data
+		request.
+		return "OrderProcessed";
+	}
+
+}
